5c486aca01c61c234ab788fb19ab35505d9033cd
DougyShy <90754920+DougyShy@users.noreply.github.com>
2025-02-17 18:15:02 -0600

    initial stable commit

---
+++ dating-app-backend/server.js
@@ -0,0 +1,56 @@
+import express from "express";
+import mongoose from "mongoose";
+import Cors from "cors";
+import Cards from "./dbCards.js";
+
+//App Config
+const app = express();
+const port = process.env.PORT || 8001;
+const connection_url =
+  "mongodb+srv://dougscheible:fossil69@cluster1.2l0ue.mongodb.net/datingDB?retryWrites=true&w=majority&appName=dating-app-mern";
+
+// Middleware
+app.use(express.json());
+app.use(Cors());
+
+//DB Config
+mongoose.connect(connection_url, {
+  //useNewUrlParser: true,
+  //useUnifiedTopology: true,
+});
+
+//API Endpoints
+app.get("/", (req, res) => res.status(200).send("Hello TheWebDev"));
+
+app.post("/dating/cards", async (req, res) => {
+  /*const dbCard = req.body;
+  Cards.create(dbCard, (err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(201).send(data);
+    }
+  });*/
+  const dbCard = req.body;
+  Cards.create(dbCard)
+    .then((result) => {
+      console.log("HERE BILLY");
+      res.send(result);
+    })
+    .catch((err) => {
+      res.send(err);
+    });
+});
+
+app.get("/dating/cards", async (req, res) => {
+  Cards.find((err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(200).send(data);
+    }
+  });
+});
+
+//Listener
+app.listen(port, () => console.log(`Listening on localhost: ${port}`));
---
+++ src/components/DatingCards.js
@@ -0,0 +1,61 @@
+import React, { useState, useEffect } from "react";
+import TinderCard from "react-tinder-card";
+import "./DatingCards.css";
+import axios from "./axios.js";
+
+const DatingCards = () => {
+  const [people, setPeople] = useState([
+    {
+      name: "Random Guy",
+      imgUrl:
+        "https://mymodernmet.com/wp/wp-content/uploads/2019/09/100k-ai-faces-6.jpg",
+    },
+    {
+      name: "Random Girl",
+      imgUrl:
+        "https://www.pixelstalk.net/wp-content/uploads/2016/05/Beautiful-Girl-Hd-Wallpapers-1080p-Images.jpg",
+    },
+    {
+      name: "Another Guy",
+      imgUrl:
+        "https://cdn.quotesgram.com/img/85/87/1731438144-BcogIEmIEAA3O5m.jpg",
+    },
+    {
+      name: "Another Girl",
+      imgUrl: "https://cdn.wallpapersafari.com/87/7/8QCtNK.jpg",
+    },
+  ]);
+
+  const swiped = (direction, nameToDelete) => {
+    console.log("receiving " + nameToDelete);
+  };
+
+  const outOfFrame = (name) => {
+    console.log(name + " left the screen!!");
+  };
+
+  return (
+    <div className="datingCards">
+      <div className="datingCards__container">
+        {people.map((person) => (
+          <TinderCard
+            className="swipe"
+            key={person.name}
+            preventSwipe={["up", "down"]}
+            onSwipe={(dir) => swiped(dir, person.name)}
+            onCardLeftScreen={() => outOfFrame(person.name)}
+          >
+            <div
+              style={{ backgroundImage: `url(${person.imgUrl})` }}
+              className="card"
+            >
+              <h3>{person.name}</h3>
+            </div>
+          </TinderCard>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default DatingCards;
